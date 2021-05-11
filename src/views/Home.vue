fefc43c75285c9ee96af9fcc5a66f866dc7fa8ed
Your Name <you@example.com>
2021-05-11 20:11:06 +0800

    aaa

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Tab/>
+    <Aaa/>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Tab from '../components/Tab'
+import Aaa from '../components/Aaa.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,Tab,
+    Aaa,
   }
 }
 </script>
