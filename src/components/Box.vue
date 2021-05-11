c63105d220f3b54a2c3896d3da3151e87f1ec2ff
Your Name <you@example.com>
2021-05-11 16:49:49 +0800

    create box

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box{
+  width: 400px;
+  height: 400px;
+  background-color: honeydew;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Box/>
   </div>
 </template>
+<script>
+import Box from '../components/Box'
+export default {
+  components: { Box }
+}
+</script>
